ba6e9403f7c849b710ee26b402de99f7c772240f
Devang Bindal <devangbindal95@gmail.com>
2026-02-23 16:51:17 +0530

    Day 1 - Band Name Generator

---
+++ DAY_1/band_name_generator.py
@@ -0,0 +1,4 @@
+print("welcome to the band name generator")
+name = input("What is the naem of the city you grew up in?\n")
+pet_name = input("Whats's your pet's name?\n")
+print("your band name could be "+name+" "+pet_name)
---
+++ DAY_1/task.py
@@ -0,0 +1,20 @@
+#print("hello world\nhello world\nhello world")->new line
+#print("hello"+" "+"devang")->concatenation
+# print("hello world")->indentation error
+#input("what is your name?")->input from user
+#print("Hello "+input("What is your name?")+"!")->input function executes and returns the name and greeting will be printed
+#name = input("what is your name? ")->variable
+#print(name)->printing variable name
+
+#name = "jack"
+#print(name)
+                 #->variable can be changed 
+#name = "angela"
+#print(name)
+
+#name = input("what is your name?")
+#print(name)
+#length = len(name)
+#print(length)->len function to find length of string
+#->variable name cant start with number 
+#name error occurs when we misspell the variable names
