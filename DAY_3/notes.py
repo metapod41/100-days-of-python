8aa6c76bd16a9d9a58c7ca46795d7a565f66ebea
Devang Bindal <devangbindal95@gmail.com>
2026-02-25 00:59:37 +0530

    DAY-3 Treasure Island

---
+++ DAY_3/notes.py
@@ -0,0 +1,54 @@
+#print("welcome to the rollercoaster")
+#height = int(input("what is your height in cm?"))
+
+#if height > 120:
+#    print("you can ride the rollercoaster")
+#    age = int(input("what is your age: "))
+#    if age <= 12:
+#        print("please pay $5")
+#    elif age<=18:
+#        print("please pay $7")
+#    else:
+#        print("please pay $12")
+#else:
+#    print("sorry you cant ride the rollercoaster")
+
+# == -> assignment operator
+# %-> modulo operator -> it gives remainder
+
+#number = int(input("enter a number: "))
+#if number%2==0:
+#    print("it is an even number\n")
+#else:
+#    print("it is an odd number\n") -> if n%2==1 then n is odd if it is 0 n is even
+
+#print("Welcome to pizza deliveries!")
+#size = input("what size pizza do you want? S , M  or L: ")
+#pepperoni = input("DO you want pepperoni on your pizza? Y or N: ")
+#extra_cheese = input("Do you want extra cheese? Y or N: ")
+#bill = 0
+
+#if size == 'S':
+#    bill+=15
+#    if pepperoni == 'Y':
+#        bill+=2
+#elif size == "M":
+#    bill+=20
+#    if pepperoni == "Y":
+#        bill+=3
+#else:
+#    bill+=25
+#    if pepperoni=="Y":
+#        bill+=3
+#
+#if extra_cheese == "Y":
+#    bill+=1
+
+#print(f"please pay ${bill}\n")
+
+#and -> && 
+#or-> ||
+#not -> not True->False 
+
+# /->escape operator used in strings
+#.lower()->change the string to lower case
