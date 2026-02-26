78da214e0b6e01a6e2d9a3b94503139d8d17776b
Devang Bindal <devangbindal95@gmail.com>
2026-02-26 17:25:34 +0530

    DAY-5 password generator

---
+++ DAY_5/task.py
@@ -0,0 +1,27 @@
+#fruits = ["Apple" , "Peach" , "Pear"]
+#for fruit in fruits:
+#    print(fruit)---> for loop on lists
+
+#student_scores = [34,43,45,6,7,6,5,43,23,45,45,21,23,99,77,87,98,78,98,54,78,99,100,33,41]
+#print(sum(student_scores))---> sum function gives the total sum
+#print(max(student_scores))---> max function gives maximum of the list elements
+
+#max = 0
+#for score in student_scores:
+#    if score>max:
+#        max=score
+#print(max) ----> max function implementation
+
+#sum = 0
+#or score in student_scores:
+#    sum+=score
+#print(sum) ---> sum function implementation
+
+#for numbers in range(1,11,3):
+#    print(numbers)---> range function takes (start,end,step) it does not include end
+
+#sum = 0
+#for num in range(1,101):
+#    sum+=num
+#print(sum)---> sum of numbers from 1 to 100
+
